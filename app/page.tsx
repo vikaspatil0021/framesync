c22759f8fdfbd5de65525d44aff1bd8e4373991a
Vikas Patil <vikaspatil0021o+github@gmail.com>
2023-12-27 19:57:01 +0530

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,21 @@ import { signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  const fetchdata = async () => {
-    await fetch("/api/teams/inviteMember", {
-      method: "post",
-      body: JSON.stringify({
-        "teamId": "8189bcf3-06fe-4f0c-854d-6a7dbeabee81",
-        "email": "kvianboleto@gmail.com"
-      })
-    })
+  // const fetchdata = async () => {
+  //   await fetch("/api/teams/inviteMember", {
+  //     method: "post",
+  //     body: JSON.stringify({
+  //       "teamId": "8189bcf3-06fe-4f0c-854d-6a7dbeabee81",
+  //       "email": "kvianboleto@gmail.com"
+  //     })
+  //   })
   
-  }
+  // }
   return (
     <>
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
         All ok
-        <Button variant='secondary' onClick={fetchdata}>
+        <Button variant='secondary'>
           Invite
         </Button>
         <Button variant='secondary' onClick={() => signOut()}>
