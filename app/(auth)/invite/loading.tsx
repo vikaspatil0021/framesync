06af5aa2d26509b167a68dde5d32ae4ccfd9ec93
Vikas Patil <vikaspatil0021o+github@gmail.com>
2023-12-27 20:59:09 +0530

    added skeleton for invite components

--- app/(auth)/invite/loading.tsx
+++ app/(auth)/invite/loading.tsx
@@ -1,7 +1,6 @@
 import { LoadingContent } from "@/components/ui/invite/inviteComponents";
 
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
     return (
         <>
             <LoadingContent />
