bb1e0b3c986f2626974625c5559b2da322a00931
Vikas Patil <vikaspatil0021o+github@gmail.com>
2024-03-16 11:11:01 +0530

    fix prisma error

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const awsCdnDomain = process.env.NEXT_PUBLIC_AWS_CDN_DOMAIN?.replace('https://','')
+
 const nextConfig = {
     images: {
         remotePatterns: [
           {
             protocol: 'https',
-            hostname: process.env.NEXT_PUBLIC_AWS_CDN_DOMAIN.replace('https://',''),
+            hostname: awsCdnDomain,
             port: '',
             pathname: '/**',
           },
