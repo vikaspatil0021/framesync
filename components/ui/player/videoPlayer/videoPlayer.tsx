d652c708fe6c92355c34adcc5bff322745e7ba1b
Vikas Patil <vikaspatil0021o+github@gmail.com>
2024-05-27 19:18:02 +0530

    better loading

--- components/ui/player/videoPlayer/videoPlayer.tsx
+++ components/ui/player/videoPlayer/videoPlayer.tsx
@@ -2,7 +2,7 @@
 
 import { useMemo } from "react"
 
-import  VideoPlayerControls from "./videoControls";
+import VideoPlayerControls from "./videoControls";
 
 interface Media {
     id: string
@@ -17,20 +17,21 @@ export default function VideoPlayer({
 }) {
 
     const src: string = useMemo(() => `${process.env.NEXT_PUBLIC_AWS_CDN_DOMAIN}/${media?.key}`, [media?.key]);
-    if (!media) return "loading";
-    
+
     return (
         <>
             <div className="flex justify-center sm:items-center sm:h-[calc(100vh-200px)] sm:p-3">
-                <div className="relative max-h-[550px]">
-                    <video id='video-player' controls={false} className="max-h-[550px] aspect-video bg-black">
-                        <source
-                            src={src}
-                            type="video/mp4"
-                        />
-                    </video>
-                    <VideoPlayerControls />
-                </div>
+                {!media ?
+                    <div className="aspect-video h-full w-full sm:w-auto flex justify-center items-center bg-[#000] max-h-[550px]"> Loading0021 ...</div>
+                    : <div className="relative max-h-[550px] h-full w-full sm:w-auto">
+                        <video id='video-player' className="max-h-[550px] h-full w-full aspect-video bg-black">
+                            <source
+                                src={src}
+                                type="video/mp4"
+                            />
+                        </video>
+                        <VideoPlayerControls />
+                    </div>}
             </div>
 
         </>
